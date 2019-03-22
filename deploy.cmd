b0e525b9fa6de3efdb922b7f97c9938a646d43e4
Matthew Dunham <matt@matthewdunham.net>
2019-03-21 19:35:56 -0500

    updated deployment configuration

--- deploy.cmd
+++ deploy.cmd
@@ -91,6 +91,13 @@ goto :EOF
 echo Verifing Node
 call :SelectNodeVersion
 
+:: 4: KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  echo Starting deployment.
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;node_modules;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
 :: 3. Install Yarn packages
 echo Installing NPM Packages.
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
@@ -98,22 +105,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   call :ExecuteCmd npm install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
-)
-
-echo Installing Build Packages.
-pushd "%DEPLOYMENT_SOURCE%"
-call :ExecuteCmd npm install
-IF !ERRORLEVEL! NEQ 0 goto error
-call :ExecuteCmd npm build
-IF !ERRORLnpmEVEL! NEQ 0 goto error
-popd
-
-
-:: 4: KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  echo Starting deployment.
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\build" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
