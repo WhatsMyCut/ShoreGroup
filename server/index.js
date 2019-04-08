ab76e5e0a4889771992fc35b6ee797616e9dda4d
Matthew Dunham <matt@matthewdunham.net>
2019-04-08 13:46:32 -0500

    cleaned up middlewares

--- server/index.js
+++ server/index.js
@@ -2,25 +2,19 @@
 
 const express = require('express');
 const logger = require('./logger');
-
 const argv = require('./argv');
 const port = require('./port');
 const setup = require('./middlewares/frontendMiddleware');
 const { resolve } = require('path');
 const app = express();
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
-
-// In production we need to pass these values in instead of relying on webpack
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
   publicPath: '/',
 });
 
-// get the intended host and port number, use localhost and port 3000 if not provided
 const customHost = argv.host || process.env.HOST;
-const host = customHost || null; // Let http.Server use its default IPv6/4 host
+const host = customHost || null;
 const prettyHost = customHost || 'localhost';
 
 // use the gzipped bundle
@@ -30,7 +24,7 @@ app.get('*.js', (req, res, next) => {
   next();
 });
 
-// Start your app.
+// Start the nodejs server
 app.listen(port, host, async err => {
   if (err) {
     return logger.error(err.message);
--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -5,16 +5,7 @@
  * Front-end middleware
  */
 module.exports = (app, options) => {
-  const isProd = process.env.NODE_ENV === 'production';
-
-  if (isProd) {
-    const addProdMiddlewares = require('./addProdMiddlewares');
-    addProdMiddlewares(app, options);
-  } else {
-    const webpackConfig = require('../../webpack/webpack.dev.babel');
-    const addDevMiddlewares = require('./addDevMiddlewares');
-    addDevMiddlewares(app, webpackConfig);
-  }
-
+  const addProdMiddlewares = require('./addProdMiddlewares');
+  addProdMiddlewares(app, options);
   return app;
 };
