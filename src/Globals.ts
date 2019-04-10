4fd9f6b56cd1a2196a3789138315bd524aaf18c2
Matthew Dunham <matt@matthewdunham.net>
2019-04-10 12:20:10 -0500

    fixed imported UI types

--- src/Globals.ts
+++ src/Globals.ts
@@ -1,60 +1,64 @@
-﻿import { IServiceUser } from "./models/IServiceUser";
-import { NSerializeJson } from "nserializejson";
-import { INodeSession } from "./models/INodeSession";
+﻿import { IServiceUser } from './models/IServiceUser';
+import { NSerializeJson } from 'nserializejson';
+import { INodeSession } from './models/INodeSession';
 
 /**
  * Contains global isomorphic session.
  */
 export default class Globals {
+  private static isInitialized: boolean = false;
 
-    private static isInitialized: boolean = false;
+  private static data: INodeSession = {};
 
-    private static data: INodeSession = {};
+  public static reset(): void {
+    this.isInitialized = false;
+    this.data = {};
+  }
 
-    public static reset(): void {
-        this.isInitialized = false;
-        this.data = {};
+  public static init(data: INodeSession): void {
+    if (this.isInitialized) {
+      throw Error('Globals is already initialized.');
     }
+    this.data = (data || {
+      public: {},
+      private: {},
+    }) as INodeSession;
+    this.isInitialized = true;
 
-    public static init(data: INodeSession): void {
-        if (this.isInitialized) {
-            throw Error("Globals is already initialized.");
-        }
-        this.data = (data || {
-            public: {}, private: {}
-        }) as INodeSession;
-        this.isInitialized = true;
+    // Use dot notation in name of the form inputs.
+    NSerializeJson.options.useDotSeparatorInPath = true;
+  }
 
-        // Use dot notation in name of the form inputs.
-        NSerializeJson.options.useDotSeparatorInPath = true;
-    }
+  private static throwIfNotInitialized() {
+    if (!this.isInitialized)
+      throw Error(
+        'Globals is not initialized. You have to call Session.init before.',
+      );
+  }
 
-    private static throwIfNotInitialized() {
-        if (!this.isInitialized)
-            throw Error("Globals is not initialized. You have to call Session.init before.");
-    }
+  public static getData(): INodeSession {
+    this.throwIfNotInitialized();
+    return this.data;
+  }
 
-    public static getData(): INodeSession {
-        this.throwIfNotInitialized();
-        return this.data;
-    }
+  public static setData(data: INodeSession) {
+    this.throwIfNotInitialized();
+    var oldData = this.data;
+    this.data = { ...oldData, ...data };
+  }
 
-    public static setData(data: INodeSession) {
-        this.throwIfNotInitialized();
-        var oldData = this.data;
-        this.data = { ...oldData, ...data };
-    }
+  public static get serviceUser(): IServiceUser {
+    let data = this.getData();
+    return data && data.public && data.public.serviceUser
+      ? data.public.serviceUser
+      : null;
+  }
 
-    public static get serviceUser(): IServiceUser {
-        let data = this.getData();
-        return (data && data.public && data.public.serviceUser) ? data.public.serviceUser : null;
-    }
+  public static set serviceUser(serviceUser: IServiceUser) {
+    this.setData({ public: { serviceUser } });
+  }
 
-    public static set serviceUser(serviceUser: IServiceUser) {
-        this.setData({ public: { serviceUser } });
-    }
-
-    public static get isAuthenticated(): boolean {
-        return this.serviceUser != null;
-    }
+  public static get isAuthenticated(): boolean {
+    return this.serviceUser != null;
+  }
 }
