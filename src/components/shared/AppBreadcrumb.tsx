5775f7e36d17cb7cb96300bdeb252064154085e0
Mike Taylor <mike@whatsmycut.com>
2019-04-24 20:54:36 -0700

    Adding Job ID in upper right

--- src/components/shared/AppBreadcrumb.tsx
+++ src/components/shared/AppBreadcrumb.tsx
@@ -7,14 +7,19 @@ import {
   IDividerAsProps,
 } from 'office-ui-fabric-react/lib/Breadcrumb';
 import { TooltipHost } from 'office-ui-fabric-react/lib/components/Tooltip';
+import { IJobModel } from '../../models/IJobModel';
 
 export interface IProps {
   show: boolean;
+  job?: IJobModel;
+  location?: string;
 }
 
 export default class AppBreadcrumb extends AppComponent<IProps, {}> {
+  private location: any;
   constructor(props) {
     super(props);
+    this.location = (window as any).location;
   }
 
   private _onBreadcrumbItemClicked = (
@@ -37,25 +42,47 @@ export default class AppBreadcrumb extends AppComponent<IProps, {}> {
       </TooltipHost>
     );
   };
+
+  private _getItems() {
+    let items = [
+      {
+        text: 'Dashboard',
+        key: 'home',
+        onClick: this._onBreadcrumbItemClicked,
+      },
+      { text: 'Jobs', key: 'f1', onClick: this._onBreadcrumbItemClicked },
+    ];
+
+    if (this.props.job) {
+      const jobId = this.props.job.Id || '–';
+      const jobName = this.props.job.Name || '–';
+      console.log('breadcrumb', jobId);
+      items.push({ text: jobName, key: 'jobid', onClick: () => undefined });
+    }
+    return items;
+  }
+
   render() {
     var css = { display: 'none' };
     if (!isNode()) {
-      css = { display: this.props.show ? 'block' : 'none' };
+      css = { display: this.props.show ? 'flex' : 'none' };
     }
+    const items = this._getItems();
     return (
       <div className="app-breadcrumb" style={css}>
-        <Breadcrumb
-          items={[
-            {
-              text: 'Dashboard',
-              key: 'home',
-              onClick: this._onBreadcrumbItemClicked,
-            },
-            { text: 'Jobs', key: 'f1', onClick: this._onBreadcrumbItemClicked },
-          ]}
-          styles={{ itemLink: { fontSize: 14 }, root: { fontSize: 14 } }}
-          dividerAs={this._getCustomDivider}
-        />
+        <div className="app-breadcrumb-items">
+          <Breadcrumb
+            items={items}
+            styles={{
+              itemLink: { fontSize: 14 },
+              root: { margin: 0, padding: 0, fontSize: 14 },
+            }}
+            dividerAs={this._getCustomDivider}
+          />
+        </div>
+        <div className="app-breadcrumb-jobid nobr right smalltext">
+          Job ID: {this.props.job.Id}
+        </div>
       </div>
     );
   }
