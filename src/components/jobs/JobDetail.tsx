7e0e27d6d6ad7651520f02a7c06b3a2ba5a8cb86
Mike Taylor <mike@whatsmycut.com>
2019-04-25 23:14:10 -0700

    WIP: UI updates to JobDetail panels

--- src/components/jobs/JobDetail.tsx
+++ src/components/jobs/JobDetail.tsx
@@ -1,9 +1,11 @@
 import '../../styles/jobs.scss';
 import React, { Component, MouseEvent } from 'react';
 import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
-import { IJobModel } from '../../models/IJobModel';
+import { IJobModel, IJobType } from '../../models/IJobModel';
 import Attachment from '../../components/attachments/Attachment';
 import { IAttachmentModel } from '../../models/IAttachmentModel';
+import { List } from 'office-ui-fabric-react/lib/components/List';
+import Moment from 'moment';
 
 export interface IProps {
   disabled?: boolean;
@@ -11,7 +13,10 @@ export interface IProps {
   job: IJobModel;
   currentTab?: string;
 }
-
+export interface IListItem {
+  Name?: string;
+  Value?: string;
+}
 export interface IState {
   currentTab?: string;
 }
@@ -19,6 +24,8 @@ export const State: IState = {
   currentTab: 'general',
 };
 export default class JobDetail extends Component<IProps, IState> {
+  private _list: List<IJobModel>;
+
   constructor(props: IProps, state: IState) {
     super(props);
     this.state = state;
@@ -95,16 +102,62 @@ export default class JobDetail extends Component<IProps, IState> {
     );
   }
 
+  private _resolveList = (list: List<IListItem>): void => {
+    this._list = list;
+  };
+
+  private _getPageHeight(idx: number): number {
+    return 50;
+  }
+
+  private _onRenderCell = (item: IListItem, index: number): JSX.Element => {
+    return (
+      <div data-is-focusable={true}>
+        <div className={'item-cell'}>
+          {index} &nbsp; {item.Name}
+        </div>
+        <div className={'item-cell'}>{item.Value}</div>
+      </div>
+    );
+  };
+
+  private _renderList(items: any): JSX.Element {
+    const { job } = this.props;
+
+    return (
+      <div className="job-details-panel">
+        <List
+          items={items}
+          getPageHeight={this._getPageHeight}
+          onRenderCell={this._onRenderCell}
+        />
+      </div>
+    );
+  }
+
   render() {
     const { job } = this.props;
     const { currentTab } = this.state;
-    let disabled = false;
-    let checked = false;
+    const jobName = job ? job.Name : '–';
+    const dueDate = Moment(job.DueDate).format('l');
+    const account = job.Account ? job.Account.Name : '–';
+    const jobType = job.Type ? job.Type.Name : '–';
+    const jobDesc = job ? job.Description : '–';
+    const tasks = job.Tasks ? job.Tasks : [];
     let generalActive = currentTab === 'general' || '' ? 'active' : '';
     let attachmentsActive = currentTab === 'attachments' ? 'active' : '';
     let tasksActive = currentTab === 'tasks' ? 'active' : '';
-    let jobName = job ? job.Name : '–';
-    let jobType = job && job.Type ? job.Type.Name : '–';
+
+    const items = job
+      ? [
+          { 'Job Name': jobName } as IListItem,
+          { Customer: account } as IListItem,
+          { 'Job Type': jobType } as IListItem,
+          { 'Job Description': jobDesc } as IListItem,
+          { 'Job Due Date': dueDate } as IListItem,
+        ]
+      : ([] as IListItem[]);
+
     return (
       <div className="job-detail-container">
         <div className="job-detail-main">
@@ -119,26 +172,21 @@ export default class JobDetail extends Component<IProps, IState> {
           />
           <div className="job-detail-panel">
             <div className={'general ' + generalActive}>
-              <div>
-                <div>Job Name</div>
-                <div>{jobName}</div>
-              </div>
-              <div>
-                <h5>Job Type</h5>
-                <p>{jobType}</p>
-              </div>
+              {this._renderList(items)}
             </div>
             <div className={'attachments ' + attachmentsActive}>
               {this._renderAttachments()}
             </div>
             <div className={'tasks ' + tasksActive}>
-              <p>tasks</p>
+              {this._renderList(tasks)}
             </div>
           </div>
         </div>
         <div className={'job-detail-comments'}>
           <h5>Comments</h5>
-          <p>[PLACEHOLDER]</p>
+          <div className="job-detail-comments-list">
+            {this._renderList(tasks)}
+          </div>
         </div>
       </div>
     );
