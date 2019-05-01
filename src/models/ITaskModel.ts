030393cd01bf36223b75bddd92e5727c2cff3c9e
Mike Taylor <mike@whatsmycut.com>
2019-05-01 01:04:15 -0700

    match ITaskModel fields to DB payload

--- src/models/ITaskModel.ts
+++ src/models/ITaskModel.ts
@@ -1,5 +1,35 @@
+import { IAttachmentModel } from './IAttachmentModel';
+import { IJobStatusReason, IJobType } from './IJobModel';
+export interface IPriority {
+  Name?: string;
+  Value?: number;
+}
 export interface ITaskModel {
-    taskID?: number;
-    name?: string;
-    description?: string;
+  Attachment?: IAttachmentModel;
+  CASS?: boolean;
+  CASSInstructions: null;
+  CreatedOn?: string;
+  Dedupe?: boolean;
+  DueDate?: string;
+  FedexAccount: null;
+  Household?: boolean;
+  HouseholdInstructions: null;
+  Id: string;
+  ModifiedOn?: string;
+  NCOA?: boolean;
+  NCOAInstructions: null;
+  Presort?: boolean;
+  Priority: IPriority;
+  ProofInstructions: null;
+  ProofType: null;
+  SpotColorMatch?: boolean;
+  SpotColorMatchInstructions: null;
+  StatusReason: IJobStatusReason;
+  Subject?: string;
+  TaskNumber: null;
+  Type: IJobType;
+  UPSAccount: null;
+  USPSAccount: null;
+  VariableProgramming?: boolean;
+  VariableProgrammingInstructions: null;
 }
